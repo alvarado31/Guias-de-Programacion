28693d59c236d746a2b03c1f03cf934517e42df7
Gerardo Alvarado <47786584+alvarado31@users.noreply.github.com>
2019-04-13 23:37:59 -0600

    Add files via upload
    Esta es mi Tercera guia Clases y objetos

---
+++ Clases y objetos/Clases y objetos/Class1.vb
@@ -0,0 +1,142 @@
+﻿Public Class Holamundo
+    Public Subsaludar()
+    Sub saludar()
+        Console.WriteLine("hola mundo")
+        Console.ReadLine()
+    End Sub
+End Class
+
+Public Class carro
+    Private marca As String
+    Private modelo As String
+    Private anho As String
+    Private color As String
+
+    Public Sub New(marca, modelo, anho, color)
+        Me.marca = marca
+        Me.modelo = modelo
+        Me.anho = anho
+        Me.color = color
+    End Sub
+
+    Public Sub mostrar()
+        Console.WriteLine("Marca: " & marca)
+        Console.WriteLine("Modelo: " & modelo)
+        Console.WriteLine("Anho: " & anho)
+        Console.WriteLine("Color: " & color)
+
+    End Sub
+
+End Class
+Public Class vehiculo_b
+    Private marca As String
+    Private modelo As String
+    Private anho As String
+    Private color As String
+
+    Public Property _marca() As String
+        Get
+            Return marca
+        End Get
+        Set(value As String)
+            marca = value
+        End Set
+    End Property
+
+    Public Property _modelo() As String
+        Get
+            Return modelo
+        End Get
+        Set(value As String)
+            modelo = value
+        End Set
+    End Property
+
+    Public Property _anho() As String
+        Get
+            Return anho
+        End Get
+        Set(value As String)
+            anho = value
+        End Set
+    End Property
+    Public Property _color() As String
+        Get
+            Return color
+        End Get
+        Set(value As String)
+            color = value
+        End Set
+    End Property
+    Public Sub devolverdatos()
+        Console.WriteLine("marca " & marca)
+        Console.WriteLine("modelo " & modelo)
+        Console.WriteLine("anho " & anho)
+        Console.WriteLine("color " & color)
+    End Sub
+End Class
+
+Public Class Class1
+    Private numcuenta As Integer
+    Private balance As Double
+    Public Sub New(numcuenta, balance)
+        Me.numcuenta = numcuenta
+        Me.balance = balance
+
+    End Sub
+    Public Sub depositar(total As Double)
+        balance = balance + total
+    End Sub
+
+    Public Sub retirar(total As Double)
+        balance = balance - total
+    End Sub
+
+    Public Sub mostrar()
+        Console.WriteLine("mostrar balance" & balance)
+        Console.ReadLine()
+    End Sub
+End Class
+Public Class calculadora
+    Public Function suma()
+        Console.WriteLine("ingrese el primer numero")
+        Dim num1 As Double = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine("ingrese el segundo numero numero")
+        Dim num2 As Double = Console.ReadLine()
+        Dim sumar As Double = num1 + num2
+        Console.WriteLine("la suma es " & sumar)
+    End Function
+
+    Public Function restar()
+        Console.WriteLine("ingrese el primer numero")
+        Dim num1 As Double = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine("ingrese el segundo numero numero")
+        Dim num2 As Double = Console.ReadLine()
+        Dim sumar As Double = num1 - num2
+        Console.WriteLine("la resta es " & sumar)
+    End Function
+
+    Public Function multiplicacion()
+        Console.WriteLine("ingrese el primer numero")
+        Dim num1 As Double = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine("ingrese el segundo numero numero")
+        Dim num2 As Double = Console.ReadLine()
+        Dim sumar As Double = num1 * num2
+        Console.WriteLine("la multiplicacion es " & sumar)
+    End Function
+
+    Public Function divicion()
+        Console.WriteLine("ingrese el primer numero")
+        Dim num1 As Double = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine("ingrese el segundo numero numero")
+        Dim num2 As Double = Console.ReadLine()
+        Dim sumar As Double = num1 / num2
+        Console.WriteLine("la divicion es " & sumar)
+    End Function
+
+End Class
+
---
+++ Clases y objetos/Clases y objetos/Module1.vb
@@ -0,0 +1,84 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim saludo As Holamundo = New Holamundo()
+        saludo.saludar()
+    End Sub
+
+
+    Sub Main()
+        Dim carro1 As carro = New carro("hilux", "4x4", "2019", "blanco")
+        Dim carro2 As carro = New carro("audi", "r8", "20117", "megro")
+        Dim carro3 As carro = New carro("yamaha", "r3", "2015", "azul con blanca")
+
+        carro1.mostrar()
+        Console.WriteLine("************************************")
+        carro2.mostrar()
+        Console.WriteLine("************************************")
+        carro3.mostrar()
+        Console.WriteLine("************************************")
+        Console.ReadLine()
+
+    End Sub
+    Sub Main()
+        Dim carro1 As vehiculo_b = New vehiculo_b
+        carro1._marca = "hilux"
+        carro1._modelo = "4x4"
+        carro1._anho = "2019"
+        carro1._color = "blanco"
+        Dim carro2 As vehiculo_b = New vehiculo_b
+        carro2._marca = "audi"
+        carro2._modelo = "r8"
+        carro2._anho = "20117"
+        carro2._color = "NEGRO"
+        Dim carro3 As vehiculo_b = New vehiculo_b
+        carro3._marca = "YAMAHA"
+        carro3._modelo = "r3"
+        carro3._anho = "2017"
+        carro3._color = "NEGRO CON BLANCA"
+
+        carro1.devolverdatos()
+        Console.WriteLine("********************************************")
+        carro2.devolverdatos()
+        Console.WriteLine("********************************************")
+        carro3.devolverdatos()
+        Console.WriteLine("********************************************")
+        Console.ReadLine()
+    End Sub
+    Sub Main()
+        Dim cuenta As Class1 = New Class1(10, 54)
+        cuenta.depositar(534)
+        cuenta.retirar(10)
+        cuenta.mostrar()
+
+    End Sub
+    Sub Main()
+
+        Console.WriteLine("calculadora selecione lo que quiere hacer")
+        Console.WriteLine("1.sumar")
+        Console.WriteLine("2.restar")
+        Console.WriteLine("3.multiplicar")
+        Console.WriteLine("4.dividir")
+        Console.WriteLine("presione cualquier otro numero para salir")
+        Dim respuesta As Integer = Console.ReadLine
+        Console.WriteLine()
+        While respuesta < 4 And respuesta > 0
+
+            Select Case respuesta
+                Case 1
+                    Dim sumarr As calculadora = New calculadora
+                    sumarr.suma()
+                    Continue While
+                Case 2
+                    Dim restarr As calculadora = New calculadora
+                    restarr.restar()
+                Case 3
+                    Dim multi As calculadora = New calculadora
+                    multi.multiplicacion()
+                Case 4
+                    Dim div As calculadora = New calculadora
+                    div.divicion()
+            End Select
+        End While
+    End Sub
+End Module
